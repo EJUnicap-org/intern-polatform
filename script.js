35013c0d034fe5c53829562ee2843fa9659388dc
rossiniroberto52 <toquehuk@gmail.com>
2026-05-06 20:07:09 -0300

    Phoenix hotfix#2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ==========================================
 const API_BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://127.0.0.1:8000'
-    : 'https://api.ejunicap.com.br';
+    : 'http://api.ejunicap.com.br';
 
 function decodificarJWT(token) {
     try {
